68421e805f425192dfbebe5b44dee8f8fd5b938d
Elukwal <elukwal3@gmail.com>
2018-10-15 07:53:28 +0300

    add child class Testconfig

--- config.py
+++ config.py
@@ -28,3 +28,6 @@ class Config:
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:21336622@localhost/pitch_test'
