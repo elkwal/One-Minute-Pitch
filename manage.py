e0ba67153ef6ce6ed95a7cbf28454a78acb153d4
Elukwal <elukwal3@gmail.com>
2018-10-15 08:05:29 +0300

    add the function test

--- manage.py
+++ manage.py
@@ -11,3 +11,8 @@ migrate = Migrate(app, db)
 manager.add_command('server', Server)
 manager.add_command('db', MigrateCommand)
 @manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
