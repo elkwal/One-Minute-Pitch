da29c24a712f0fcb3f114617862a0720e666afa2
Elukwal <elukwal3@gmail.com>
2018-10-15 08:08:43 +0300

    add the function make_shell_context

--- manage.py
+++ manage.py
@@ -18,3 +18,5 @@ def test():
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User,Pitch=Pitch,Comment=Comment)
