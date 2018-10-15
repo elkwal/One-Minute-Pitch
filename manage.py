843b9e591eb9820a9444b767b2b071033f675246
Elukwal <elukwal3@gmail.com>
2018-10-15 08:03:07 +0300

    add create_app

--- manage.py
+++ manage.py
@@ -2,3 +2,5 @@ from app import create_app, db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User,Pitch,Comment
+# instances for the create_app
+app = create_app('production')
